7472aec4193815ebab979cf3148c0694e4df59aa
hamidb80 <hr.bolouri@gmail.com>
2022-03-23 20:40:43 +0430

    add slice test

--- tests/test.nim
+++ tests/test.nim
@@ -1,16 +1,20 @@
-import std/[unittest]
+import std/[unittest, sequtils]
 import macroplus {.all.}
 
 suite "intuitive indexing":
-  const r = c 4..^1
-  
+  const r = c 3..^1
+
   test "formal":
     check:
-      r[0] == 4
-      r[1] == 5
+      r[0] == 3
+      r[1] == 4
 
   test "backward":
     check:
       r[^1] is BackwardsIndex
       r[^1].int == 1
       r[^2].int == 2
+
+  test "slice":
+    check:
+      toseq(1..10)[r] == @[4, 5, 6, 7, 8, 9, 10]
